07f770b2749421759a22872e80d5fff91a82c942
Rajendra Ayer <rajendra.ayer@outlook.com>
2025-05-23 19:54:03 +1000

    barchart added

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="sewak app")
-
-
-file=pd.read_csv("data/amazon.csv")
-
-st.write("Hello All")
-
-st.write(file)
+file = st.file_uploader("Upload CSV", type="csv")
+if file:
+    df=pd.read_csv(file)
+    st.write(df.head())
+    st.bar_chart(df.iloc[8])
+else:
+    st.write("Please Upload file to see magic")
